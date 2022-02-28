afd1101a1f70a7fb8e9f4bf7cd27c73a79aa31a7
evan-forbes <evan.samuel.forbes@gmail.com>
2022-02-28 00:23:19 -0600

    estimate square size and move erasure step to the app

--- x/payment/types/payformessage_test.go
+++ x/payment/types/payformessage_test.go
@@ -70,7 +70,7 @@ func TestNextPowerOf2(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		res := nextPowerOf2(tt.input)
+		res := NextPowerOf2(tt.input)
 		assert.Equal(t, tt.expected, res)
 	}
 }
